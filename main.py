b21b6134d5cfe4b2c9b8b1070c79db1313b0d3f6
AndrewMur <95580456+AndrewMuratov@users.noreply.github.com>
2022-03-29 00:28:43 +0000

    stuff. man idfk what i changed and i wont bother to check. hope this helps lmao

--- main.py
+++ main.py
@@ -8,13 +8,11 @@ server.keep_running()
 
 list_of_messages = ["Always believe that something wonderful will probably never happen",
                     "Due to recent cutbacks, the light at the end of the tunnel has been turned off",
-                    "Until you spread your wings, you will never know that you can\'t fly",
+                    "Until you spread your wings, you won\'t' know that you can\'t fly",
                     "Always remember that you are unique - just like everybody else",
                     "Everything happens for a reason. Sometimes the reason is that you\'re stupid and make bad decisions",
                    "You\'re only as good as your last fuck-up.",
                    "Going outside is highly overrated"]
-list_of_time_options = [1800,
-                        3600]
 
 def choose_message(lst_msg):
   random_msg = random.choice(lst_msg)
@@ -34,5 +32,5 @@ server.login(sender_email, sender_password)
 for i in range(7):
   message = 'Subject: {}\n\n{}'.format(f"Day {i + 1}", f'{choose_message(list_of_messages)}')
   server.sendmail(sender_email, reciever_email, message)
-  print(f"{i + 1} emails sent!")
-  time.sleep(86400)
+  print("message sent")
+  time.sleep(5)
